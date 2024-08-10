03007123ff48351d8af519742e24ce534a174a71
oeportil <ernestoportilllo09@gmail.com>
2024-08-10 13:13:25 -0600

    Agregar archivos de proyecto.

---
+++ TareasMVC/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+using TareasMVC.Models;
+using TareasMVC.Servicios;
+using TareasMVC.Entidades;
+
+namespace TareasMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> localizer;
+        private readonly ApplicationDbContext context;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public HomeController(ILogger<HomeController> logger,
+            IStringLocalizer<HomeController> localizer,
+            ApplicationDbContext context, IServicioUsuarios servicioUsuarios)
+        {
+            _logger = logger;
+            this.localizer = localizer;
+            this.context = context;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            Console.WriteLine(usuarioId);
+            var tareas = await context.Tareas
+                .Where(t => t.UsuarioCreacionId == usuarioId)
+            .OrderBy(t => t.Orden).ToListAsync();
+            return View(tareas);
+        }
+
+        public IActionResult CrearTarea()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CrearTarea(Tarea taremodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(taremodel);
+            }
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            taremodel.UsuarioCreacionId = usuarioId;
+            await context.Tareas.AddAsync(taremodel);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> EditarTarea(int id)
+        {
+            var resultado = await context.Tareas.Where(x => x.Id == id).FirstOrDefaultAsync(); 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditarTarea(Tarea modelo)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CambiarIdioma(string cultura, string urlRetorno)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cultura)),
+                 new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(5) }
+                );
+
+            return LocalRedirect(urlRetorno);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TareasMVC/Models/TareaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TareasMVC.Models
+{
+    public class TareaDTO
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        // public string Descripcion { get; set; }
+        public int PasosRealizados { get; set; }
+        public int PasosTotal { get; set; }
+    }
+}
